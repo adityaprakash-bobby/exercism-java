cad1f84dd27e49129310efd55bc533e62a6a8934
Aditya Prakash <adityaprakash.bobby@gmail.com>
2024-01-03 01:48:26 +0530

    (misc) add pangram

---
+++ src/main/java/org/example/PangramChecker.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class PangramChecker {
+
+    public boolean isPangram(String input) {
+        return input.toLowerCase().chars().filter(Character::isLetter).distinct().count() == 26;
+    }
+}
