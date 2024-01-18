4dfe963e8c7867bdd0893786ec1ac65c5a7bfb63
Aditya Prakash <adityaprakash.bobby@gmail.com>
2024-01-18 19:43:40 +0530

    (misc) add pop count

---
+++ src/main/java/org/example/PopCount.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class PopCount {
+    public int eggCount(int number) {
+        int count = 0;
+        while (number > 0) {
+            if ((number & 1) == 1) count++;
+
+            number >>= 1;
+        }
+
+        return count;
+    }
+}
+
