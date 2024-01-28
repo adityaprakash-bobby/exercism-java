5953abf36e72f3606a8fb24eac8b388a74c6be10
Aditya Prakash <adityaprakash.bobby@gmail.com>
2024-01-28 18:27:47 +0530

    (misc) add forth evaluator

--- src/main/java/org/example/ForthEvaluator.java
+++ src/main/java/org/example/ForthEvaluator.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 class ForthEvaluator {
 
-    private static final String SUBSTITUTION_REGEX = "^:\\s([a-zA-Z-]+)\\s([a-zA-Z]+)\\s;$";
+    private static final String SUBSTITUTION_REGEX = "^:\\s([a-zA-Z0-9/+*-]+)\\s(([a-zA-Z0-9/+*-]+\\s)+);$";
 
     private static final Set<String> validManipulationKeywords = Set.of("dup", "drop", "over", "swap");
     private static final Set<String> validArithmeticKeywords = Set.of("+", "-", "*", "/");
@@ -73,6 +73,9 @@ class ForthEvaluator {
 
     // stack manipulations
     private static void dup(Stack<Integer> stack) {
+        if (stack.empty())
+            throw new IllegalArgumentException("Duplicating requires that the stack contain at least 1 value");
+
         stack.add(stack.peek());
     }
 
@@ -109,38 +112,21 @@ class ForthEvaluator {
 
     private static void applyArithmeticToken(Stack<Integer> stack, String token) {
         switch (token) {
-            case "+":
-                add(stack);
-                break;
-            case "-":
-                subtract(stack);
-                break;
-            case "*":
-                multiply(stack);
-                break;
-            case "/":
-                division(stack);
-            default:
-                throw new IllegalArgumentException("Unsupported arithmetic operation");
+            case "+" -> add(stack);
+            case "-" -> subtract(stack);
+            case "*" -> multiply(stack);
+            case "/" -> division(stack);
+            default -> throw new IllegalArgumentException("Unsupported arithmetic operation");
         }
     }
 
     private static void applyManipulationToken(Stack<Integer> stack, String token) {
         switch (token) {
-            case "dup":
-                dup(stack);
-                break;
-            case "drop":
-                drop(stack);
-                break;
-            case "swap":
-                swap(stack);
-                break;
-            case "over":
-                over(stack);
-                break;
-            default:
-                throw new IllegalArgumentException("Unsupported manipulation operation");
+            case "dup" -> dup(stack);
+            case "drop" -> drop(stack);
+            case "swap" -> swap(stack);
+            case "over" -> over(stack);
+            default -> throw new IllegalArgumentException("Unsupported manipulation operation");
         }
     }
 
@@ -160,32 +146,87 @@ class ForthEvaluator {
         else if (isValidNumeric(token))
             applyNumericToken(stack, token);
         else
-            throw new IllegalArgumentException("invalid keyword token `" + token + "` found");
+            throw new IllegalArgumentException("No definition available for operator \"" + token + "\"");
     }
 
-    private static String getSubstitutionValue(String input) {
+    private static Substitution getSubstitutionValue(String input) {
         Pattern p = Pattern.compile(SUBSTITUTION_REGEX);
         Matcher m = p.matcher(input);
+
+        String replace, with;
         if (m.find()) {
-            
+            replace = m.group(1);
+            with = m.group(2).trim();
+
+            if (isValidNumeric(replace))
+                throw new IllegalArgumentException("Cannot redefine numbers");
+
+            return new Substitution(replace, with);
         }
+
+        return null;
     }
+
+    public static void main(String[] args) {
+        ForthEvaluator fe = new ForthEvaluator();
+        System.out.println(fe.evaluateProgram(List.of("1 2 dup swap over - + -")));
+
+//        System.out.println(getSubstitutionValue(": asd asd asd fsd ;"));
+    }
+
+    private String buildEffectiveEvaluateString(String evaluateString, List<String> substitutions) {
+
+        Map<String, String> subtitutionMap = new HashMap<>();
+
+        String effectiveEvaluateString = evaluateString;
+        String replace, with;
+        for (String substitution : substitutions) {
+            Substitution s = getSubstitutionValue(substitution);
+
+            if (s == null) throw new IllegalArgumentException("Invalid substitution");
+
+            replace = s.replace;
+            with = s.with;
+
+            if (subtitutionMap.containsKey(replace))
+                with = with.replace(replace, subtitutionMap.get(replace));
+
+            String[] withTokens = with.split("\\s+");
+            for (String withToken : withTokens) {
+                if (subtitutionMap.containsKey(withToken))
+                    with = with.replace(withToken, subtitutionMap.get(withToken));
+            }
+
+            subtitutionMap.put(s.replace, with);
+        }
+
+        for (Map.Entry<String, String> s : subtitutionMap.entrySet()) {
+            replace = s.getKey();
+            with = s.getValue();
+            effectiveEvaluateString = effectiveEvaluateString.replace(replace, with);
+        }
+
+        return effectiveEvaluateString;
+    }
+
     List<Integer> evaluateProgram(List<String> input) {
+        String toBeEvaluated = null;
+        List<String> substitutions = new ArrayList<>();
+        for (String i: input) {
+            if (isSubstitutionDefinition(i)) substitutions.add(i.toLowerCase());
 
+            toBeEvaluated = i.toLowerCase();
+        }
 
-        if (input.size() == 0) return Collections.emptyList();
+        toBeEvaluated = buildEffectiveEvaluateString(toBeEvaluated, substitutions);
 
-        String[] tokens = input.get(0).split(" ");
+        String[] tokens = toBeEvaluated.split("\\s+");
         Stack<Integer> stack = new Stack<>();
         Arrays.stream(tokens).forEach(token -> applyToken(stack, token));
 
         return stack;
     }
 
-
-//    public static void main(String[] args) {
-//        ForthEvaluator fe = new ForthEvaluator();
-//        System.out.println(fe.evaluateProgram(List.of("1 2 dup swap over - + -")));
-//
-//    }
+    private record Substitution(String replace, String with) {
+    }
 }
